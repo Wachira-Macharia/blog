638ba3d952b0ee6a6d91ffa4b7eacc380607561b
Wachira-Macharia <eugenecomboni@gmail.com>
2025-08-12 11:34:52 +0300

    implement MapStruct mappers for categories

---
+++ src/main/java/com/comboni/blog/mappers/CategoryMapper.java
@@ -0,0 +1,29 @@
+package com.comboni.blog.mappers;
+
+import com.comboni.blog.domain.PostStatus;
+import com.comboni.blog.domain.dtos.CategoryDto;
+import com.comboni.blog.domain.entities.Category;
+import com.comboni.blog.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+
+    @Mapping(target = "postCount" , source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto(Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
