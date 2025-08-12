f19e8bcc00148bcbe33f5b9e466c6514b9db4dc7
Wachira-Macharia <eugenecomboni@gmail.com>
2025-08-12 11:24:58 +0300

    initial Spring Boot & PostgreSQL Setup

---
+++ src/test/java/com/comboni/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comboni.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
