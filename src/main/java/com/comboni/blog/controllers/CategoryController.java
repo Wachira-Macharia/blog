cba33f482a5e1f512d3d57393c44257199c25f64
Wachira-Macharia <eugenecomboni@gmail.com>
2025-08-12 11:35:37 +0300

    add REST controllers and endpoints for categories

---
+++ src/main/java/com/comboni/blog/controllers/CategoryController.java
@@ -0,0 +1,32 @@
+package com.comboni.blog.controllers;
+
+import com.comboni.blog.domain.dtos.CategoryDto;
+import com.comboni.blog.domain.entities.Category;
+import com.comboni.blog.mappers.CategoryMapper;
+import com.comboni.blog.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> listCategories() {
+        List<CategoryDto> categories = categoryService.listCategories()
+                .stream()
+                .map(categoryMapper::toDto)
+                .toList();
+
+        return ResponseEntity.ok(categories);
+    }
+}
